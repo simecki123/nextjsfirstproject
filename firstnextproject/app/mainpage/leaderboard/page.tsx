17ee9a8e7468dac7f0619b1775235536834dd525
Šime Rončević <sime.roncevic@kodelab.hr>
2024-07-09 14:21:47 +0200

    Updating table of the users view

--- firstnextproject/app/mainpage/leaderboard/page.tsx
+++ firstnextproject/app/mainpage/leaderboard/page.tsx
@@ -4,7 +4,10 @@ import React from 'react'
 export default function Leaderboard() {
   return (
     <div>
+      <div className='flex justify-center items-center min-h-screen '>
         <UserListComponent />
+      </div>
+        
     </div>
   )
 }

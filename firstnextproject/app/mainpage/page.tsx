4a97300d22893f1d6884a0051984dac9c0dc673a
Andrija Skontra <askontra22@student.unizd.hr>
2024-07-09 09:54:29 +0200

    add brew component/page

--- firstnextproject/app/mainpage/page.tsx
+++ firstnextproject/app/mainpage/page.tsx
@@ -1,8 +1,13 @@
+'use client'
 import React from 'react';
 import MainPageComponent from '../components/mainpage-components/mainpage';
 import BrewCoffeeComponent from '../components/mainpage-components/brew-coffee-components';
+import { useRouter } from 'next/navigation';
 
 export default function MainPage() {
+
+  const router = useRouter();
+
   return (
     <div>
       <MainPageComponent /> {/* This component acts as a header */}
@@ -10,7 +15,8 @@ export default function MainPage() {
         {/* Centering the BrewCoffeeComponent */}
         <div className="flex items-center justify-center mt-4">
           <div className="shadow hover:shadow-lg transform hover:scale-105
-          p-4 bg-white transition duration-300 rounded-2xl cursor-pointer mb-96">
+          p-4 bg-white transition duration-300 rounded-2xl cursor-pointer mb-96"
+            onClick={() => { router.push('/mainpage/brew') }}>
             <BrewCoffeeComponent />
           </div>
         </div>

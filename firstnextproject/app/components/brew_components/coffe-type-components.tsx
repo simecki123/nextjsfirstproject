f6a440f3f224c54fbdd4e595be726c1e1c327d6a
Andrija Skontra <askontra22@student.unizd.hr>
2024-07-09 14:26:53 +0200

    improve order coffee component

--- firstnextproject/app/components/brew_components/coffe-type-components.tsx
+++ firstnextproject/app/components/brew_components/coffe-type-components.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+
 export default function CoffeeTypeComponent() {
 
   const values = [0, 1, 2, 3, 4, 5]
+
+  const [selectedAmountOfMilk, setAmountOfMilk] = useState<number | null>(null);
+  const [selectedAmountOfSugar, setAmountOfSugar] = useState<number | null>(null);
 
   return (
     <div className="bg-gray-200 py-4 px-4 rounded-xl">
@@ -19,13 +24,15 @@ export default function CoffeeTypeComponent() {
 
           <div className="flex flex-row items-center">
             {values.map(valueSugar => (
-              <SugarButton value={valueSugar} />
+              <SugarButton value={valueSugar} isSelected={selectedAmountOfSugar === valueSugar}
+                onSelect={setAmountOfSugar} />
             ))}
           </div>
 
           <div className="flex flex-row items-center">
             {values.map(valueMilk => (
-              <MilkButton value={valueMilk} />
+              <MilkButton value={valueMilk} isSelected={selectedAmountOfMilk === valueMilk}
+                onSelect={setAmountOfMilk} />
             ))}
           </div>
         </div>
@@ -35,6 +42,7 @@ export default function CoffeeTypeComponent() {
           <img src="/sugar-cube.png" width={50} height={30} className="ml-6 mb-5 mt-3"></img>
           <img src="/milk-carton.png" width={50} height={30} className="ml-6"></img>
         </div>
+
       </div>
 
       <div className="flex justify-center mt-4">
@@ -45,17 +53,53 @@ export default function CoffeeTypeComponent() {
   );
 }
 
-function SugarButton({ value }: { value: number }) {
+interface AmountProp {
+  value: number,
+  isSelected: boolean,
+  onSelect: (value: number) => void,
+}
+
+// function SugarButton({ value, isSelected, onSelect }: AmountProp) {
+//   return (
+//     <button className="rounded-xl w-8 h-8 flex items-center justify-center cursor-pointer bg-blue-400
+//       m-1 hover:scale-105 transform transition hover:bg-blue-500 shadow-xl">{value}</button>
+//   )
+// }
+
+function SugarButton({ value, isSelected, onSelect }: AmountProp) {
   return (
-    <button className="rounded-xl w-8 h-8 flex items-center justify-center cursor-pointer bg-blue-400
-      m-1 hover:scale-105 transform transition hover:bg-blue-500 shadow-xl">{value}</button>
+    <button
+      className={
+        `rounded-xl w-8 h-8 flex items-center justify-center cursor-pointer 
+          m-1 hover:scale-105 transform transition shadow-xl 
+          ${isSelected ? 'bg-blue-500 text-white' : 'bg-blue-400 hover:bg-blue-500'}
+        `}
+      onClick={() => onSelect(value)}>{value}</button>
   )
 }
 
-function MilkButton({ value }: { value: number }) {
+function MilkButton({ value, isSelected, onSelect }: AmountProp) {
   return (
-    <button className="rounded-xl w-8 h-8 flex items-center justify-center cursor-pointer bg-purple-400
-      m-1 hover:scale-105 transform transition hover:bg-purple-500 shadow-xl">{value}</button>
+    <button
+      className={
+        `rounded-xl w-8 h-8 flex items-center justify-center cursor-pointer 
+          m-1 hover:scale-105 transform transition shadow-xl 
+          ${isSelected ? 'bg-purple-500 text-white' : 'bg-purple-400 hover:bg-purple-500'}
+        `}
+      onClick={() => onSelect(value)}>{value}</button>
+  )
+}
+
+function TimeButton({ time, isSelected, onSelect }: TimeButtonProps) {
+  return (
+    <div
+      className={`w-16 h-16 flex items-center justify-center rounded cursor-pointer
+        hover:shadow-lg transform hover:scale-105 transition-all
+        ${isSelected ? 'bg-green-400 text-white' : 'bg-green-300 hover:bg-green-400'}`}
+      onClick={() => onSelect(time)}
+    >
+      <span>{time}</span>
+    </div>
   )
 }
 

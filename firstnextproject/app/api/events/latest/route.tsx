72bd934a2f6896b0177e21f6c8e29b7502b0a7fc
Šime Rončević <sime.roncevic@kodelab.hr>
2024-07-11 13:50:10 +0200

    Removing mongodb

--- firstnextproject/app/api/events/latest/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { eventCollection } from "../route";
-
-export async function GET() {
-	const lastEvent = await eventCollection.find().sort({ _id: -1 }).limit(1).toArray();
-	console.log(lastEvent);
-	return new NextResponse(JSON.stringify(lastEvent), { status: 200 })
-}

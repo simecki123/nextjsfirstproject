b71445f86dfce4afae84f2f78a08130df02e791e
Andrija Skontra <askontra22@student.unizd.hr>
2024-07-12 15:37:44 +0200

    implement notifications

--- firstnextproject/app/components/AcceptCoffeComponents/AcceptCoffeComponent.tsx
+++ firstnextproject/app/components/AcceptCoffeComponents/AcceptCoffeComponent.tsx
@@ -2,8 +2,9 @@
 import React from 'react';
 import coffeIcon from '@/public/coffeImage.png';
 import { useRouter } from 'next/navigation';
+import { Event } from '@/app/mainpage/notification/page';
 
-export default function AcceptCoffeComponent() {
+export default function AcceptCoffeComponent({ event }: { event: Event }) {
 
     const router = useRouter();
 
@@ -11,23 +12,16 @@ export default function AcceptCoffeComponent() {
         router.push('/mainpage');
     }
 
-    const user = {
-        firstName: "Teo",
-        lastName: "Jakšić",
-        coffeNumber: 10,
-        score: 4.5,
-    };
-
     return (
         <div className='border max-w-sm border-gray-300 shadow-lg rounded-lg p-4 bg-stone-200'>
             <div className='flex justify-between items-center mb-4'>
-                <p className='text-lg font-semibold'>{user.firstName} is going to make coffee in 5 minutes. Do you want one too?</p>
+                <p className='text-lg font-semibold'>{event.firstName} {event.lastName} is going to make coffee in 5 minutes. Do you want one too?</p>
                 <img src={coffeIcon.src} alt="Coffee icon" className='h-11 w-11' />
             </div>
             <div className='flex justify-between'>
                 <button className='bg-green-500 text-white py-3 px-5 rounded-lg
                     hover:bg-green-600 transition duration-300 mx-1
-                    hover:scale-105' onClick={() => router.push('/mainpage/choose')}>✔️</button>
+                    hover:scale-105' onClick={() => router.push(`/mainpage/notification/${event.eventId}`)}>✔️</button>
                 <button className='bg-red-500 text-white py-3 px-5 rounded-lg
                     hover:bg-red-600 transition duration-300 mx-1
                     hover:scale-105' onClick={pushMainpage}>❌</button>
